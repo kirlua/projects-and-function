e74c375645419a0ff41e756d94c24b42cf46be61
kirlua <98923556+kirlua@users.noreply.github.com>
2023-04-11 10:00:01 +1200

    ff
    ff

---
+++ list_of_combo.py
@@ -0,0 +1,7 @@
+import easygui
+# dictionary
+combo = {
+    "VALUE": {"items": {"Beef burger": 5.69, "Fries": 1.00, "Fizzy drink": 1.00}},
+    "CHEEZY": {"items": {"Cheeseburger": 6.69, "Fries": 1.00, "Fizzy drink": 1.00}},
+    "SUPER": {"items": {"Cheeseburger": 6.69, "Large fries": 2.00, "Smoothie": 2.00}}
+}
